772b05074bfa061bb2ff4138e83d29ef9ff80b5a
Bumper0417 <chrisgoros13@gmail.com>
2020-01-20 17:44:13 +0000

    Added editrecipe.html

--- app.py
+++ app.py
@@ -38,18 +38,48 @@ def insert_recipe():
     # I want to transform it as a list (array)
 
     # I get the value :
+    recipe_method = my_user_data['recipe_method']
+    recipe_method_as_an_array = recipe_method.split(",")
     recipe_ingredients = my_user_data['recipe_ingredients']         # string
     recipe_ingredients_as_an_array = recipe_ingredients.split(",")  # list (each item is separated by a ",")
     recipe_allergens = my_user_data['recipe_allergens']
     recipe_allergens_as_an_array = recipe_allergens.split(",")
 
+    #remove the \r\n from the recipe_method
+    recipe_method = [method.replace("\r\n","") for method in recipe_method]
+
     # Update the dictionary key's value :
-    my_user_data['recipe_ingredient'] = recipe_ingredients_as_an_array
+    my_user_data['recipe_ingredients'] = recipe_ingredients_as_an_array
     my_user_data['recipe_allergens'] = recipe_allergens_as_an_array
-    
+    my_user_data['recipe_method'] = recipe_method_as_an_array
+
     # Send it to the database: 
     recipes.insert_one(my_user_data)
 
+    return redirect(url_for('get_recipes'))
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe, categories=all_categories)
+
+@app.route('/update_recipe/<recipe_id>', methods=['POST'])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)}, 
+    {
+        'recipe_name' : request.form.get('recipe_name'),
+        'category_name' : request.form.get('category_name'),
+        'image_url' : request.form.get('image_url'),
+        'recipe_ingredients' : request.form.get('recipe_ingredients'),
+        'recipe_method' : request.form.get('recipe_method'),
+        'recipe_country_of_origin' : request.form.get('recipe_country_of_origin'),
+        'recipe_vegeterian' : request.form.get('recipe_vegeterian'),
+        'recipe_vegan' : request.form.get('recipe_vegan'),
+        'recipe_allergens' : request.form.get('recipe_allergens'),
+        'recipe_nutricion' : request.form.get('recipe_nutricion')
+    })
     return redirect(url_for('get_recipes'))
 
 
